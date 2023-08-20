0f3a78edfbdd048f47a82c9eacedf5716dd4f43e
Wiesiol <77128711+Wiesiol@users.noreply.github.com>
2023-08-20 17:39:38 +0200

    forwardPoint

---
+++ Project/Assets/Scripts/Utils/ForwardPoint.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class ForwardPoint : MonoBehaviour
+{
+    [SerializeField] private Vector3 offset;
+    private Vector3 pointInFront;
+
+    private void Update()
+    {
+        pointInFront = transform.position + (transform.forward * offset.z) + (transform.right * offset.x) + transform.up * offset.y;
+        Debug.Log(transform.forward);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireSphere(pointInFront, 2);
+    }
+}
