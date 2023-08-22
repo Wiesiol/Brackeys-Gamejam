ca3f3f404386670cac8f1608d69c293891496fbb
Wiesiol <77128711+Wiesiol@users.noreply.github.com>
2023-08-22 20:06:33 +0200

    Laserowa dzida

---
+++ Project/Assets/Scripts/Player/Laser.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    private LineRenderer lineRenderer;
+    private Transform[] points;
+
+    private void DrawLaser()
+    {
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
